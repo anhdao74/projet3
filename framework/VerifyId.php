40195661da862d00004fee698d61b62d5578384e
anhdao <anhdao.lelievre@gmail.com>
2018-03-18 12:44:05 +0100

    ajout des méthod getAddCommentId et getAddChapterId

--- framework/VerifyId.php
+++ framework/VerifyId.php
@@ -53,6 +53,61 @@ class VerifyId
         }          
 	}
 
+	public function getAddCommentId()
+	{
+		if (array_key_exists('id',$_GET))
+            {
+                if (!empty($_GET['id']))
+                {
+                    if (ctype_digit($_GET['id']))
+                    {
+                        $commentManager = new CommentManager();
+                        $comment = $commentManager->getComment($_GET['id']);
+                        if ($comment == TRUE)
+                        {
+                            $req = new FlashMessageSession();
+                            $flash = $req->asMessage();
+                            $flash = $req->setFlash('L\'identifiant de ce commentaire existe déja');
+                            header('Location: index.php');
+                            exit();
+                        }
+                        else
+                        {
+                            $req = new FlashMessageSession();
+                            $flash = $req->asMessage();
+                            $flash = $req->setFlash('Le commentaire demandé n\'existe pas');
+                            header('Location: index.php');
+                            exit();
+                        }
+                    }
+                    else
+                    {
+                        $req = new FlashMessageSession();
+                        $flash = $req->asMessage();
+                        $flash = $req->setFlash('L\'identifiant d\'un commentaire doit être un nombre entier');
+                        header('Location: index.php');
+                        exit(); 
+                    }
+                }
+                else
+                {
+                    $req = new FlashMessageSession();
+                    $flash = $req->asMessage();
+                    $flash = $req->setFlash('L\'identifiant du commentaire ne peut être vide');
+                    header('Location: index.php');
+                    exit();
+                }
+            }
+        else
+        {
+            $req = new FlashMessageSession();
+            $flash = $req->asMessage();
+            $flash = $req->setFlash('Aucun identifiant de commentaire envoyé');
+            header('Location: index.php');
+            exit();
+        }          
+	}
+
 	public function getChapterId()
 	{
 		if (array_key_exists('id',$_GET))
@@ -100,6 +155,61 @@ class VerifyId
             $flash = $req->asMessage();
             $flash = $req->setFlash('Aucun identifiant de chapitre envoyé');
             header('Location: index.php');
+            exit();
+        }          
+	}
+
+	public function getAddChapterId()
+	{
+		if (array_key_exists('id',$_GET))
+            {
+                if (!empty($_GET['id']))
+                {
+                    if (ctype_digit($_GET['id']))
+                    {
+                        $chapterManager = new ChapterManager();
+                        $chapter = $chapterManager->getChapter($_GET['id']);
+                        if ($chapter == TRUE)
+                        {
+                            $req = new FlashMessageSession();
+                            $flash = $req->asMessage();
+                            $flash = $req->setFlash('L\'identifiant de ce chapitre existe déja');
+                            header('Location: index.php?action=showAdmin');
+                            exit();
+                        }
+                        else
+                        {
+                            $req = new FlashMessageSession();
+                            $flash = $req->asMessage();
+                            $flash = $req->setFlash('Le chapitre demandé n\'existe pas');
+                            header('Location: index.php?action=showAdmin');
+                            exit();
+                        }
+                    }
+                    else
+                    {
+                        $req = new FlashMessageSession();
+                        $flash = $req->asMessage();
+                        $flash = $req->setFlash('L\'identifiant d\'un chapitre doit être un nombre entier');
+                        header('Location: index.php?action=showAdmin');
+                        exit(); 
+                    }
+                }
+                else
+                {
+                    $req = new FlashMessageSession();
+                    $flash = $req->asMessage();
+                    $flash = $req->setFlash('L\'identifiant du chapitre ne peut être vide');
+                    header('Location: index.php?action=showAdmin');
+                    exit();
+                }
+            }
+        else
+        {
+            $req = new FlashMessageSession();
+            $flash = $req->asMessage();
+            $flash = $req->setFlash('Aucun identifiant de chapitre envoyé');
+            header('Location: index.php?action=showAdmin');
             exit();
         }          
 	}
