62cf5f33f19e4e4170fcb8db3a6ea180b332267f
anhdao <anhdao.lelievre@gmail.com>
2018-03-18 11:37:18 +0100

    appel de la method getCommentId de la classe VerifyId

--- controller/CommentController.php
+++ controller/CommentController.php
@@ -49,91 +49,41 @@ function addComment()
         {
             throw new Exception('Aucun identifiant de billet envoyé');
         }
-    header('Location: index.php?action=chapter&id=' . strip_tags($_POST['id']));
-    exit();
+        header('Location: index.php?action=chapter&id=' . strip_tags($_POST['id']));
+        exit();
     }
 
 
     function removeComment()
     {
+        $verif = new VerifyId();
+        $comment = $verif-> getCommentId();
+
         $commentManager = new CommentManager();  
         $removeLine=$commentManager->cancelComment(strip_tags($_GET['id']));
-        if ($removeLine)
-        {
-            if (isset($_GET['id']))
-            {
-               $_GET['id'] = (int) $_GET['id'];
-               if ($_GET['id'] >= 1 AND $_GET['id'] <=500)
-               {
-                    $req = new FlashMessageSession();
-                    $flash = $req->asMessage();
-                    $flash = $req->setFlash('Le commentaire a bien été supprimé');
-                    header('Location: index.php?action=showAdmin');
-                    exit();
-                }  
-            }
-        }
-        else 
-        {
-            throw new Exception('Aucun identifiant de commentaire envoyé');
-        }
+
+        $req = new FlashMessageSession();
+        $flash = $req->asMessage();
+        $flash = $req->setFlash('Le commentaire a bien été supprimé');
+        header('Location: index.php?action=showAdmin');
+        exit();
+               
     }
 
     function signaledComment()
     {
-        if (array_key_exists('id',$_GET))
-            {
-                if (!empty($_GET['id']))
-                {
-                    if (ctype_digit($_GET['id']))
-                    {
-                        $commentManager = new CommentManager();
-                        $comment = $commentManager->getComment($_GET['id']);
-                        if ($comment == TRUE)
-                        {
-                            $signaled = $commentManager->getSignaledComment(strip_tags($_GET['id']));
+        $verif = new VerifyId();
+        $comment = $verif-> getCommentId();
 
-                            $req = new FlashMessageSession();
-                            $flash = $req->asMessage();
-                            $flash = $req->setFlash('Votre message a bien été signalé');
-                            
-                            header('Location: index.php?action=chapter&id=' . strip_tags($comment['chapterId']));
-                            exit();
-                        }
-                        else
-                        {
-                            $req = new FlashMessageSession();
-                            $flash = $req->asMessage();
-                            $flash = $req->setFlash('Le commentaire demandé n\'existe pas');
-                            header('Location: index.php');
-                            exit();
-                        }
-                    }
-                    else
-                    {
-                        $req = new FlashMessageSession();
-                        $flash = $req->asMessage();
-                        $flash = $req->setFlash('L\'identifiant d\'un commentaire doit être un nombre entier');
-                        header('Location: index.php');
-                        exit(); 
-                    }
-                }
-                else
-                {
-                    $req = new FlashMessageSession();
-                    $flash = $req->asMessage();
-                    $flash = $req->setFlash('L\'identifiant ne peut être vide');
-                    header('Location: index.php');
-                    exit();
-                }
-            }
-        else
-        {
-            $req = new FlashMessageSession();
-            $flash = $req->asMessage();
-            $flash = $req->setFlash('Aucun identifiant de commentaire envoyé');
-            header('Location: index.php');
-            exit();
-        }           
+        $commentManager = new CommentManager();
+        $signaled = $commentManager->getSignaledComment(strip_tags($_GET['id']));
+
+        $req = new FlashMessageSession();
+        $flash = $req->asMessage();
+        $flash = $req->setFlash('Votre message a bien été signalé');
+        
+        header('Location: index.php?action=chapter&id=' . strip_tags($comment['chapterId']));
+        exit();
+                        
     }
 }
